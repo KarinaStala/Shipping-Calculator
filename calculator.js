59edc684998f9a6c4c1aff27a790bf1f7b2ba69c
Karina <karina.stala@gmail.com>
2023-05-01 23:40:49 +0200

    Calculator commit

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function calculateTotal() {
+        const inputElement = document.querySelector(".js-cost-input");
+        let cost = Number(inputElement.value);
+
+        if (cost < 40) {
+          cost = cost + 10;
+        }
+        document.querySelector(".js-total-cost").innerHTML = `$${cost}`;
+      }
+
+      function handleCostKeydown(event) {
+        if (event.key === "Enter") {
+          calculateTotal();
+        }
+      }
